4245b4f5a29464261c2622d09831c540d5c38d24
xentqul <ciragromova@yandex.ru>
2025-06-16 20:28:24 +0300

    env2

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -3,8 +3,13 @@ const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
+console.log('SUPABASE_URL:', supabaseUrl); // Для проверки
+console.log('SUPABASE_KEY:', supabaseKey); // Для проверки
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase credentials are missing in environment variables');
 }
 
-module.exports = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
